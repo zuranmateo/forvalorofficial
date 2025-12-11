46fccbf66b0566c3805a818e48a55e26d3c74fd7
zuranmateo <mateo.zuran@scv.si>
2025-12-11 08:12:50 +0100

    sanitylive fix

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -8,7 +8,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 
 const builder = imageUrlBuilder(client);
--- sanity/lib/write-client.ts
+++ sanity/lib/write-client.ts
@@ -8,7 +8,7 @@ export const writeClient = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  useCdn: false,
   token,
 });
 
